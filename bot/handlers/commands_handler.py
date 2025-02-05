03f24a2c046272657ab19626886bd05dcd2ec745
Diamond Land <81316675+DiamondLand@users.noreply.github.com>
2025-02-05 20:20:28 +0300

    finish

--- bot/handlers/commands_handler.py
+++ bot/handlers/commands_handler.py
@@ -1,10 +1,12 @@
+import psutil
+
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.fsm.context import FSMContext
 from aiogram.utils.markdown import hlink
 
-from database.services import get_or_create_user_service
+from database.services import get_or_create_user_service, get_users_service
 
 from functions.greeting import send_greeting
 
@@ -59,6 +61,22 @@ async def info_cmd(message: Message, state: FSMContext):
         text=message_text,
         reply_markup=support_button().as_markup()
     )
+
+
+# --- Отправка статистики --- #
+@router.message(Command("bin"))
+async def statistic_cmd(message: Message):    
+    if int(message.chat.id) in map(int, message.bot.ADMIN_CHATS):
+        users_data = await get_users_service()
+        if users_data:
+            users_count = f"<b>Пользователей:</b> <code>{len(users_data)}</code>"
+
+            await message.answer(
+                text=f"<b>СТАТИСТИКА:</b>\
+                    \n\n{users_count}\
+                    \n\n<b>CPU:</b> <code>{psutil.cpu_percent(interval=1)}</code> | <b>RAM:</b> <code>{psutil.virtual_memory().percent}</code>%\
+                    \n<b>Использовано дискового пространства:</b> <code>{psutil.disk_usage('/').percent}</code>%"
+            )
 
 
 # --- Перейти в рассылку -> Написать текст --- #
